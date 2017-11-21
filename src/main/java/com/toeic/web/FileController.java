2f56e1b21d760f06cde4991206b38e74f52ed455
'SooBokJin' <thdnthdn24@gmail.com>
2017-11-21 12:11:15 +0900

    cross origin setting

---
+++ src/main/java/com/toeic/web/FileController.java
@@ -0,0 +1,61 @@
+package com.toeic.web;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.java.Log;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/file/*")
+@Log
+public class FileController {
+
+	@GetMapping("/{fileName:.+}")
+	public byte[] showImg(@PathVariable("fileName") String fileName) {
+		try {
+			File file = new File("D:\\ToeicManager_repo\\" + fileName);
+			
+			return FileUtils.readFileToByteArray(file);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	@GetMapping("/newdata")
+	public List<String> getNewData() {
+		
+		File file = new File("D:\\ToeicManager_repo\\academy\\admy_data.txt");
+		List<String> list = new ArrayList<String>();
+				
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			
+			while (br.read() != -1) {
+				list.add(br.readLine());
+			}
+			
+			log.info("LIST: "+list);
+			return list;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
--- src/main/java/com/toeic/web/WordController.java
+++ src/main/java/com/toeic/web/WordController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.toeic.dto.WordDTO;
 import com.toeic.service.WordService;
 
+@CrossOrigin
 @RestController
 public class WordController {
 
