2915fda054e1ae658e9c80978bf22dcf249de52c
'SooBokJin' <thdnthdn24@gmail.com>
2017-12-02 21:48:09 +0900

    review 및 cycle(exam) 로직 구현 완료

--- src/main/java/com/toeic/dto/CycleDTO.java
+++ src/main/java/com/toeic/dto/CycleDTO.java
@@ -13,13 +13,17 @@ public class CycleDTO {
 
 	private int dno;
 	private int wno;
+	private int vno;
+	private String word;
+	private String meaning;
 	private String mid;
-	private int t1; //첫번째 시험
-	private int t2; //...
-	private int t3;
-	private int t4;
-	private int t5;
+	private String t1; //첫번째 시험
+	private String t2; //...
+	private String t3;
+	private String t4;
+	private String t5;
 	private int score; //해당 단어에 대한 암기 점수
 	private Date nextCycle; //다음 시험날짜
-	
+	private Date lastExamDate; //마지막으로 시험 친 날짜
+	private Date regdate; //등록된 날짜
 }
